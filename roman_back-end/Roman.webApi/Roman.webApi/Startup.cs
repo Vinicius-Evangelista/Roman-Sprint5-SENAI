2372e063fe3907ae20aeda59ee490e799f482a08
52645548890 <52645548890@DESKTOP-9K54HD5>
2021-11-26 15:53:49 -0300

    swagger feito

--- roman_back-end/Roman.webApi/Roman.webApi/Startup.cs
+++ roman_back-end/Roman.webApi/Roman.webApi/Startup.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Roman.webApi
@@ -18,7 +21,17 @@ namespace Roman.webApi
         {
             services
                .AddControllers();
+
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Roman.webApi", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+            });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,6 +40,15 @@ namespace Roman.webApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+           {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Roman.webApi");
+               c.RoutePrefix = string.Empty;
+           });
 
             app.UseRouting();
 
