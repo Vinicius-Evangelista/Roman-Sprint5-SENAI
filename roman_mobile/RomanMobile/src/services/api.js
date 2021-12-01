6cb5ad04b7686554a4c8a3782355fffaf9b21a15
Joaoopeedro <joaopedronr37@gmail.com>
2021-12-01 17:23:10 -0300

    splashscreen

--- roman_mobile/RomanMobile/src/services/api.js
+++ roman_mobile/RomanMobile/src/services/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
 
-baseURL: 'http://192.168.1.112:5000/api'
-
-// api joao
-//  baseURL: 'http://192.168.5.152:5000/api'
-
+    
+    
+    baseURL: 'http://192.168.5.152:5000/api'
+    
+//baseURL: 'http://192.168.1.112:5000/api'
 // api rafa
 // baseURL: 'http://192.168.15.34:5000/api'
 // baseURL: 'http://192.168.4.55:5000/api' 
